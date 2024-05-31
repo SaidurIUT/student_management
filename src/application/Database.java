14047599a05501cca5a54c8ef9e9fbf663b49fbc
SaidurIUT <saidurrahman@iut-dhaka.edu>
2024-05-31 11:47:53 +0600

    new repo

---
+++ src/application/Database.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+
+	public static Connection connectDB() {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/university", "root", "123");
+			return connect;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
